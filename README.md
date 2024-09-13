b2506d0b0d359b4ef5893cd5a9b8d9ba27cf2a6b
seonhoshin <sunlike0508@gmail.com>
2024-09-13 23:55:27 +0900

    검증2

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # 검증
 
 ## BingingResult
+
+스프링이 제공하는 검증 오류를 보관하는 객체이다. 검증 오류가 발생하면 여기 보관
+
+`BindingResult` 가 있으면 `@ModelAttribute` 에 데이터 바인딩 시 오류가 발생해도 컨트롤러가 호출된다!
+
+
+
+**예) @ModelAttribute에 바인딩 시 타입 오류가 발생하면?**
+
+`BindingResult` 가 없으면 400 오류가 발생하면서 컨트롤러가 호출되지 않고, 오류 페이지로 이동한다. 
+
+`BindingResult` 가 있으면 오류 정보( `FieldError` )를 `BindingResult` 에 담아서 컨트롤러를 정상 호출한다.
+
+**BindingResult에 검증 오류를 적용하는 3가지 방법**
+
+`@ModelAttribute` 의 객체에 타입 오류 등으로 바인딩이 실패하는 경우 스프링이 `FieldError` 생성해서`BindingResult  에 넣어준다.
+
+개발자가 직접 넣어준다.
+
+`Validator`
+
+**타입 오류 확인**
+
+숫자가 입력되어야 할 곳에 문자를 입력해서 타입을 다르게 해서 `BindingResult` 를 호출하고 `bindingResult` 의 값을 확인해보자.
+
+**주의**
+
+`BindingResult` 는 검증할 대상 바로 다음에 와야한다. 순서가 중요하다. 
+
+예를 들어서 `@ModelAttribute Item item` , 바로 다음에 `BindingResult` 가 와야 한다.
+
+`BindingResult` 는 Model에 자동으로 포함된다.
+
+**BindingResult와 Errors** 
+
+`org.springframework.validation.Errors` 
+
+`org.springframework.validation.BindingResult `
+
+`BindingResult` 는 인터페이스이고, `Errors` 인터페이스를 상속받고 있다.
+
+실제 넘어오는 구현체는 `BeanPropertyBindingResult` 라는 것인데, 둘다 구현하고 있으므로 `BindingResult` 대신에 `Errors` 를 사용해도 된다. 
+
+`Errors` 인터페이스는 단순한 오류 저장과 조회 기능을 제공한다. 
+
+`BindingResult` 는 여기에 더해서 추가적인 기능들을 제공한다. 
+
+`addError()` 도 `BindingResult` 가 제공하므로 여기서는 `BindingResult` 를 사용하자. 
+
+주로 관례상 `BindingResult` 를 많이 사용한다.
+
+
---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -0,0 +1,102 @@
+package hello.itemservice.web.validation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/validation/v2/items")
+@RequiredArgsConstructor
+@Slf4j
+public class ValidationItemControllerV2 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v2/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v2/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v2/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        if(!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다"));
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", "가격은 1000원 부터 1000000까지 허용"));
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", "수량은 9999개까지 입니다"));
+        }
+
+        if(item.getQuantity() != null &&  item.getPrice() !=null) {
+
+            if(item.getQuantity() < 9999) {
+                int resultPrice = item.getPrice() * item.getQuantity();
+
+                if(resultPrice < 10000) {
+                    bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10000원 이상어야 합니다. 현재 값 = " + resultPrice));
+                }
+            }
+        }
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors= {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v2/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+}
+
